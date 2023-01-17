7820850208215a84e788f728226ebda653b3c0bd
Andrii-kyryliuk11 <poteriana@gmail.com>
2023-01-17 22:08:17 +0100

    start

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+function createGalleryMarkup(galleryItems) {
+  const arrayOfMarkup = [];
+
+  galleryItems.forEach(image => {
+    arrayOfMarkup.push(
+      `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+    );
+  });
+
+  refs.gallery.innerHTML = arrayOfMarkup.join('');
+}
+
+createGalleryMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox', function () {});
---
+++ src/js/02-video.js
@@ -0,0 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player', {});
+
+let savedTime;
+
+const localStorageObj = localStorage.getItem('videoplayer-current-time');
+function getSavedTime() {
+  if (localStorageObj) {
+    savedTime = JSON.parse(
+      localStorage.getItem('videoplayer-current-time')
+    ).seconds;
+  }
+}
+
+getSavedTime();
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+const onPlay = function (data) {
+  const stringifySavedTime = JSON.stringify(data);
+
+  localStorage.setItem('videoplayer-current-time', stringifySavedTime);
+
+  console.log(stringifySavedTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
