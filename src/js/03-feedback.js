27651dc4713838f7cd8e928706d75f505d8255e0
Andrii-kyryliuk11 <111685476+Andrii-kyryliuk11@users.noreply.github.com>
2023-01-19 20:46:34 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,43 +3,72 @@ import throttle from 'lodash.throttle';
 const refs = {
   emailInput: document.querySelector('[name=email]'),
   textareaInput: document.querySelector('[name=message]'),
-  submitButton: document.querySelector('form'),
+  submitForm: document.querySelector('form'),
+  submitButton: document.querySelector('[type=submit]'),
 };
 
-refs.emailInput.addEventListener('input', onEmailFill);
-refs.textareaInput.addEventListener('input', onTextareaFill);
-refs.submitButton.addEventListener('submit', onFormSubmit);
+function allowToSubmit() {
+  if (
+    refs.emailInput.value.length !== 0 &&
+    refs.textareaInput.value.length !== 0
+  ) {
+    refs.submitButton.disabled = false;
+  } else {
+    refs.submitButton.disabled = true;
+  }
+}
+
+refs.emailInput.addEventListener('input', throttle(onEmailFill, 500));
+refs.textareaInput.addEventListener('input', throttle(onTextareaFill, 500));
+refs.submitForm.addEventListener('submit', onFormSubmit);
 
 let savedForm = localStorage.getItem('feedback-form-state');
-
-let formData = {};
+let formData;
+JSON.parse(savedForm) === null
+  ? (formData = {})
+  : (formData = JSON.parse(savedForm));
 
 formAutocomplete();
 
 function formAutocomplete() {
-  if (savedForm) {
-    refs.emailInput.value = JSON.parse(savedForm).email;
+  refs.submitButton.disabled = true;
 
-    refs.textareaInput.value = JSON.parse(savedForm).message;
+  if (savedForm) {
+    switch (undefined) {
+      case JSON.parse(savedForm).email:
+        refs.emailInput.value = '';
+        break;
+
+      default:
+        refs.emailInput.value = JSON.parse(savedForm).email;
+        allowToSubmit();
+        break;
+    }
+    switch (undefined) {
+      case JSON.parse(savedForm).message:
+        refs.textareaInput.value = '';
+        break;
+      default:
+        refs.textareaInput.value = JSON.parse(savedForm).message;
+        allowToSubmit();
+        break;
+    }
   }
 }
 
-console.log(refs.emailInput);
-console.log(refs.textareaInput);
-console.log(refs.submitButton);
-
 function writeToLocalStorage() {
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  allowToSubmit();
 }
 
 function onEmailFill(evt) {
-  const email = evt.currentTarget.value;
+  const email = evt.target.value;
   formData.email = email;
   writeToLocalStorage();
 }
 
 function onTextareaFill(evt) {
-  const message = evt.currentTarget.value;
+  const message = evt.target.value;
   formData.message = message;
   writeToLocalStorage();
 }
@@ -47,4 +76,8 @@ function onTextareaFill(evt) {
 function onFormSubmit(evt) {
   evt.preventDefault();
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData = {};
+  refs.submitButton.disabled = true;
 }
